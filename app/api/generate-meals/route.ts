89d5542d42391cfa88e5eb5f6c881888dbc046cb
Lucien Thomas <lthomas@sciport.org>
2025-03-02 19:32:05 -0600

    dev: actually fixed woweee

--- app/api/generate-meals/route.ts
+++ app/api/generate-meals/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from 'next/server';
 import { RateLimiterMemory, RateLimiterRes } from 'rate-limiter-flexible';
 
-export const maxDuration = 1;
+export const maxDuration = 60;
 
 // Create a rate limiter
 // This example allows 10 requests per IP address per minute
