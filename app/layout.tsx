270c1379f656812b30764bec3f07e270c6c9cd93
Lucien Thomas <lthomas@sciport.org>
2025-03-03 14:59:29 -0600

    dev: added speed insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import "./globals.css";
 
 import { MealProvider } from "@/contexts/meal-context";
@@ -29,6 +30,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         <Analytics />
+        <SpeedInsights />
         <MealProvider>{children}</MealProvider>
       </body>
     </html>
