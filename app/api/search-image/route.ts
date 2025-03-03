9a4bceba3cecf91285d404bd667384687e75f5bc
Lucien Thomas <lthomas@sciport.org>
2025-03-02 23:46:09 -0600

    dev: oooops

--- app/api/search-image/route.ts
+++ app/api/search-image/route.ts
@@ -2,7 +2,7 @@
 
 import { NextResponse } from 'next/server';
 
-export async function GET(request: { url: string | URL; }) {
+export async function GET(request: { url: string; }) {
   const { searchParams } = new URL(request.url);
   const query = searchParams.get('query');
   
